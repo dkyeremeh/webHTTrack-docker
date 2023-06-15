ef195fcad8307f84fdc8a39ef80ce45b9ad87aa9
Desmond Kyeremeh <elDekyfin@gmail.com>
2023-06-15 22:13:25 +0000

    Build and push

---
+++ build.sh
@@ -0,0 +1,10 @@
+function build_n_push {
+
+  docker buildx build . \
+  --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 \
+  --tag $1:$2 --tag $1:latest  \
+  --build-arg VERSION=$1
+  --push
+}
+
+build_n_push eldekyfin/webhttrack $1

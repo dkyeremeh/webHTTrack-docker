0113d9cb51a13ef92d3923a8b5485c833c734425
Desmond Kyeremeh <elDekyfin@gmail.com>
2023-06-16 00:55:05 +0000

    Remove arm/v6

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@
 
 function build_n_push {
   docker buildx create --use
-  
+
   docker buildx build . \
-  --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 \
+  --platform linux/amd64,linux/arm64,linux/arm/v7 \
   --tag $1:$2 --tag $1:latest  \
   --build-arg VERSION=$1 \
   --push
